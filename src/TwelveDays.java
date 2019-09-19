347c3154b8534439b7894321f5bd855af85b0990
batsoarr000 <batsoarr000@CAM-LAB-10.bgsd.k12.wa.us>
2019-09-19 07:27:54 -0700

    update

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -2,7 +2,52 @@
 public class TwelveDays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Twelve days of crismax
+		System.out.println("On the first day of Christmas,");
+		intro();
+		Day1();
+		System.out.println("On the second day of Christmas,");
+		intro();
+		Day2();
+		System.out.println("On the third day of Christmas,");
+		intro();
+		Day3();
+		System.out.println("On the fourth day of Christmas,");
+		intro();
+		Day4();
+		System.out.println("On the fifth day of Christmas,");
+		intro();
+		Day5();
+		System.out.println("On the six day of Christmas,");
+		intro();
+		Day6();
+		System.out.println("On the seventh day of Christmas,");
+		intro();
+		Day7();
+		System.out.println("On the eighth day of Christmas,");
+		intro();
+		Day8();
+		System.out.println("On the ninth day of Christmas,");
+		intro();
+		Day9();
+		System.out.println("On the ninth day of Christmas,");
+		intro();
+		Day10();
+		System.out.println("On the eleventh day of Christmas,");
+		intro();
+		Day11();
+		System.out.println("On the twelth day of Christmas,");
+		intro();
+		Day12();
+	}
+	public static void Day1() {
+		System.out.println("a partridge in a pear tree");
+	}
+	public static void Day2() {
+		System.out.println("two turtle doves,");
+	}
+	public static void intro() {
+		System.out.println("my true love sent to me");
 
 	}
 
