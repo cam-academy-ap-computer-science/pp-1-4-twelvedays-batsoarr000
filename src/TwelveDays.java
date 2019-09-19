f03e5593a61810106883df5e2858ef55c4366ce0
batsoarr000 <batsoarr000@CAM-LAB-10.bgsd.k12.wa.us>
2019-09-19 07:45:30 -0700

    Done

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -6,77 +6,109 @@ public class TwelveDays {
 		System.out.println("On the first day of Christmas,");
 		intro();
 		Day1();
+		System.out.println();
 		System.out.println("On the second day of Christmas,");
 		intro();
 		Day2();
+		System.out.println();
 		System.out.println("On the third day of Christmas,");
 		intro();
 		Day3();
+		System.out.println();
 		System.out.println("On the fourth day of Christmas,");
 		intro();
 		Day4();
+		System.out.println();
 		System.out.println("On the fifth day of Christmas,");
 		intro();
 		Day5();
+		System.out.println();
 		System.out.println("On the six day of Christmas,");
 		intro();
 		Day6();
+		System.out.println();
 		System.out.println("On the seventh day of Christmas,");
 		intro();
 		Day7();
+		System.out.println();
 		System.out.println("On the eighth day of Christmas,");
 		intro();
 		Day8();
+		System.out.println();
 		System.out.println("On the ninth day of Christmas,");
 		intro();
 		Day9();
+		System.out.println();
 		System.out.println("On the ninth day of Christmas,");
 		intro();
 		Day10();
+		System.out.println();
 		System.out.println("On the eleventh day of Christmas,");
 		intro();
 		Day11();
+		System.out.println();
 		System.out.println("On the twelth day of Christmas,");
 		intro();
 		Day12();
 	}
 	public static void Day1() {
-		System.out.println("a partridge in a pear tree");
+		System.out.println("a partridge in a pear tree.");
 	}
 	public static void Day2() {
-		System.out.println("two turtle doves,");
+		System.out.println("two turtle doves, and");
+		Day1();
 	}
 	public static void intro() {
 		System.out.println("my true love sent to me");
 	}
 	public static void Day3() {
-		System.out.println("");
+		System.out.println("three French hens,");
+		Day2();
+		
 	}
 	public static void Day4() {
-		System.out.println("");
+		System.out.println("four calling birds,");
+		Day3();
+		
 	}
 	public static void Day5() {
-		System.out.println("");
+		System.out.println("five golden rings,");
+		Day4();
+		
 	}
 	public static void Day6() {
-		System.out.println("");
+		System.out.println("six geese a-laying,");
+		Day5();
+		
 	}
 	public static void Day7() {
-		System.out.println("");
+		System.out.println("seven swans a-swimming,");
+		Day6();
+		
 	}
 	public static void Day8() {
-		System.out.println("");
+		System.out.println("eight maids a-milking,");
+		Day7();
+		
 	}
 	public static void Day9() {
-		System.out.println("");
+		System.out.println("nine ladies dancing,");
+		Day8();
+		
 	}
 	public static void Day10() {
-		System.out.println("");
+		System.out.println("ten lords a-leaping,");
+		Day9();
+		
 	}
 	public static void Day11() {
-		System.out.println("");
+		System.out.println("eleven pipers piping,");
+		Day10();
+		
 	}
 	public static void Day12() {
-		System.out.println("");
-	}
+		System.out.println("twelve drummers drumming,"); 
+		Day11();
+		
+		}
 }
